5b3f73cfcb0cffdfd1c26886ce29d8e7cfae2055
Maneth Pak <manethpak00@gmail.com>
2021-10-11 19:53:42 +0700

    app entry point

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+import sys
+
+from gui import MainWindow
+
+
+def run_app():
+    """Main function."""
+    app = QApplication(sys.argv)
+    view = MainWindow()
+    view.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    run_app()
